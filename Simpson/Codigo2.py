7780fcd14c5351f341849d7e3e0dcce409339328
Edu <eduedem@gift.com>
2022-10-19 14:03:19 +0200

    a

--- Simpson/Codigo2.py
+++ Simpson/Codigo2.py
@@ -1,11 +1,15 @@
 #llamar a la API
+from random import random
 from urllib import request
 import requests
 import csv
 import time
 import os
 import shutil
+import random
 
+#LISTA VACIA PARA COMPROBAR SI EL AUTOR YA HA SALIDO
+autoresfrases =[]
 
 while True:
    URL= 'https://thesimpsonsquoteapi.glitch.me/quotes'
@@ -21,35 +25,63 @@ while True:
    frase_simpson:str = datos[0]['quote']
    autor:str = datos[0]['character']
    
-   #LISTA VACIA PARA COMPROBAR SI EL AUTOR YA HA SALIDO
-   autoresfrases =[]
-
+   
    #COMPROBACIÓN DE QUE FUNCIONA LA PETICION
    print(autor)
    print(frase_simpson)
+   print(autoresfrases)
 
-   if (print(autor in autoresfrases)):
+   if (autor in autoresfrases):
     #autor ya existe
-    with open(autor, 'a', newline='') as csvfile:
-     spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-     spamwriter.writerow(frase_simpson)
+
     #descargar la imagen
+    random = random.randint(0,100)
+    imagen = str(random) + "_" + 'imagen.png'
+    f = open(imagen,'wb')
+    f.write(respuesta.content)
+    f.close()
+
+    #recorrer directorio
+    direc = 'C:\\Users\\sragg\\Documents\\GitHub\\EDEM2022\\Simpson'
+    with os.scandir(direc) as ficheros:
+      for i in ficheros:
+        if i.name == autor:
+         direc1 = os.path.join(direc,autor)
+         direc2 = "imagenes"
+         direc3 = os.path.join(direc1,direc2)
+         #mover la imagen descargada a la carpeta del autor
+         shutil.move(imagen,direc3)
+
+   
+#PROBLEMA Nº1: COMO HACER QUE ESCRIBA EN EL CSV DEL AUTOR QUE ACABA DE SALIR
+    #escribir la frase en el CSV que ya se ha creado ANTES por AUTOR
+      direc = 'C:\\Users\\sragg\\Documents\\GitHub\\EDEM2022\\Simpson'
+      with os.scandir(direc) as ficheros:
+       for i in ficheros:
+        if i.name == autor:
+         with open('autor.csv', 'a', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(frase_simpson)
+
+    #cada 30seg haga una peticion
+    time.sleep(5)
+   
 
    else:
     #autor no ha salido
+    
 
     #añadir el autor a la lista
     autoresfrases.append(autor)
+
+    print(f"{autoresfrases}\n")
 
     #crear carpeta autor
     directory = autor
     parent_dir = 'C:\\Users\\sragg\\Documents\\GitHub\\EDEM2022\\Simpson'
     path = os.path.join(parent_dir,directory)
     os.mkdir(path)
-
-    #comprobación donde se ha creado
-    print("Directory '%s' created" %directory)
 
     #crear archivo csv y guardar la frase
     with open("autor.csv", 'a', newline='') as csvfile:
@@ -62,15 +94,15 @@ while True:
 
     #crear carpeta imagenes dentro de autor
     directory1 = "imagenes"
-    parent_dir1 = 'C:\\Users\\sragg\\Documents\\GitHub\\EDEM2022\\Simpson'
-    path1 = os.path.join(parent_dir1,directory1)
+    path1 = os.path.join(path,directory1)
     os.mkdir(path1) 
 
-    #descargar la imagen en la carpeta imagenes, dentro de autor
-    imagen = datos[0]['image']
-    with open(imagen, 'wb') as handler:
-	    handler.write(respuesta)
+    #descargar la imagen
+    f = open('imagen.png','wb')
+    f.write(respuesta.content)
+    f.close()
 
     #mover la imagen descargada a la carpeta del autor
     shutil.move('imagen.png',path1)
 
+    time.sleep(5)
